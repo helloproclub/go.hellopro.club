f7e7f5027586d0ca4e9ea960bd568cba2bec0462
Afnizar Nur Ghifari <afnizarhilmi@gmail.com>
2018-04-11 17:47:27 +0700

    Added protalk 1 link

--- src/Main.js
+++ src/Main.js
@@ -17,6 +17,10 @@ app.get('/ocp1', (req, res) =>
 app.get('/lineocp', (req, res) =>
   res.redirect('http://line.me/R/ti/g/P6DOqQrunS')
 );
+``
+app.get('/protalk1', (req, res) =>
+  res.redirect('https://www.eventbrite.com/e/protalk-visual-branding-for-startup-tickets-45057584448')
+);
 
 app.listen(envs.port, () =>
   console.log('Application listening on port ' + envs.port + '!')
